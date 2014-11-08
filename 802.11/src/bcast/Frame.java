dcd09ab231171ed632250dbbaa6fc6d04991a627
Graham Baker <graham.cr.baker@gmail.com>
2014-11-08 15:55:21 -0800

    little changes to frame

--- 802.11/src/bcast/Frame.java
+++ 802.11/src/bcast/Frame.java
@@ -1,7 +1,14 @@
 package bcast;
+import java.util.*;
 
 public class Frame {
+	// whole message
+	
+	private BitSet control;
+	private int destAddr;
+	private int srcAddr;
 	private byte[] data;
+	private byte[] CRC;
 	
 	public Frame(){
 		
