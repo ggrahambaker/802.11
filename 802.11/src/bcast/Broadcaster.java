6bc2e55f1ba22115fc57357d3678de2fbc37f94d
Zeke <zrosenberg@pugetsound.edu>
2014-11-08 17:23:57 -0800

    adds sender

--- 802.11/src/bcast/Broadcaster.java
+++ 802.11/src/bcast/Broadcaster.java
@@ -6,13 +6,13 @@ private ArrayBlockingQueue sendQueue;
 
 	public Broadcaster()
 	{
-		
+		System.out.println("Broadcaster up and running.");
 	}
 
 	public int send(short dest, byte[] data, int len)
 	{
 
-		
+
 	}
 
 }
