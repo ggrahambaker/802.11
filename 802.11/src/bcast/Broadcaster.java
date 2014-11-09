39a0414941747b9832316a786ee716f9b56dc595
Graham Baker <graham.cr.baker@gmail.com>
2014-11-08 16:11:46 -0800

    changes file names

--- 802.11/src/bcast/Broadcaster.java
+++ 802.11/src/bcast/Broadcaster.java
@@ -1,10 +1,14 @@
 package bcast;
-
+import rf.RF;
 public class Broadcaster {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("WELCOME");
+		
+		Frame f = new Frame();
+		
 	}
 
 }
--- 802.11/src/bcast/Frame.java
+++ 802.11/src/bcast/Frame.java
@@ -4,13 +4,36 @@ import java.util.*;
 public class Frame {
 	// whole message
 	
-	private BitSet control;
+	private byte[] control;
 	private int destAddr;
 	private int srcAddr;
 	private byte[] data;
 	private byte[] CRC;
 	
+	
+	// constructor for CHECKPOINT 2
+	// create a frame with type Data, 
+	// a sequence number of zero, and with the retry bit set to zero (off).
 	public Frame(){
+		contInit();
 		
+	}
+	
+	private byte[] contInit(){
+		byte[] control;
+		BitSet temp = new BitSet(16);
+		// temp.set(0, true);
+		
+		System.out.println(temp.toString());
+		control = temp.toByteArray();
+		
+		return control;
+	}
+	
+	public String toString(){
+		String frame;
+		frame = "";
+		
+		return frame;
 	}
 }
