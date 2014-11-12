4daa5dc1e142c4619eb24889d01673358b1643cc
Graham Baker <graham.cr.baker@gmail.com>
2014-11-12 01:19:47 -0800

    &&&

--- 802.11/src/Tester.java
+++ 802.11/src/Tester.java
@@ -1,9 +1,26 @@
+import java.util.BitSet;
 
+import rf.RF;
 public class Tester {
 
-	public void main(String[] args){
-		System.out.println("finally");
-	}
-	
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("HAIIS");
+		
+		byte[] cs;
+		BitSet temp = new BitSet(160);
+		temp.set(0, 80, true);
+		
+		// System.out.println(temp.toString());
+		cs = temp.toByteArray();
+		
 
+		Frame f = new Frame((short)100, (short)200, cs, cs.length);
+		f.printFields();
+		System.out.println(f.toString());
+
+	}
+
+}
