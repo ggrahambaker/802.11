bcd2f6573f4d0015deccb3650fe72f495bd69a80
Zeke <zrosenberg@pugetsound.edu>
2014-11-12 17:31:08 -0800

    Added receiver, began implementing LinkLayer constructor

--- 802.11/src/LinkLayer.java
+++ 802.11/src/LinkLayer.java
@@ -7,6 +7,15 @@ private ArrayBlockingQueue sendQueue;
 	public LinkLayer()
 	{
 		System.out.println("LinkLayer up and running.");
+		
+		sendQueue = new ArrayBlockingQueue<Frame>(10);
+		RF rf = new RF(null,null);
+		
+		
+		new Thread(new Sender(sendQueue,rf));
+		new Thread(new Receiver(rf));
+		
+		while(true);
 	}
 
 	public int send(short dest, byte[] data, int len)
